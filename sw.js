4372385226856d92927b48cd7086b9f10081cc4b
wickel-formula <s.wickel@t-online.de>
2026-07-01 07:21:02 +0200

    Add files via upload
    Dateien für PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'hydranten-v1';
+const FILES = [
+  './hydranten_pruefung.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
